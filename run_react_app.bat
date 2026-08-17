b9e8a0518eb40fb867d3b546b1c832b2bdf1080c
DO-Agent <do-agent@valu.app>
2026-08-17 19:04:23 -0300

    Agregado lanzador run_react_app.bat para ejecutar la web React en 1 clic

---
+++ run_react_app.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ======================================================================
+echo INICIANDO NAVEGADOR WEB REACT DE SCRAPING INMOBILIARIO (1.12M PROPS)
+echo ======================================================================
+echo Servidor API corriendo en http://localhost:8050
+echo Abriendo navegador web predeterminado...
+start http://localhost:8050
+python server_react_api.py
+pause
